030018a47c4cf8c1d41ef8e40e8de695de07375c
HieuCVu_1050 <43056834+Hieu1050@users.noreply.github.com>
2024-04-19 16:23:24 +0700

    change from css to xpath to scrape values

--- project_1/project_1/settings.py
+++ project_1/project_1/settings.py
@@ -64,7 +64,7 @@ ROBOTSTXT_OBEY = True
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 ITEM_PIPELINES = {
    "project_1.pipelines.Project1Pipeline": 300,
-   # "project_1.pipelines.SaveToMySQLPipeLine": 400,
+   "project_1.pipelines.SaveToMySQLPipeLine": 400,
 }
 FEEDS = {
    'newsdata.json': {
--- project_1/project_1/spiders/news_spider_1_2.py
+++ project_1/project_1/spiders/news_spider_1_2.py
@@ -7,30 +7,24 @@ class NewsSpider12Spider(CrawlSpider):
     name = "news_spider_1.2"
     allowed_domains = ["thanhnien.vn"]
     start_urls = ["https://thanhnien.vn"]
-
-    rules = (
-        Rule(LinkExtractor(restrict_xpaths =r'//ul[@class = "menu-nav"]/li/a[contains(concat(" ", normalize-space(@class), " "), " nav-link ") and not(contains(@class, "home"))]'), callback="parse_nav", follow=True),
-        # Rule(LinkExtractor(restrict_xpaths =r'//*[@id="content"]//h3[contains (@class, "box-title-text")]/a'), callback="parse_article", follow=True),
-        ) 
     custom_settings = {
         'CLOSESPIDER_PAGECOUNT': 20,
     }
+    rules = (
+        Rule(LinkExtractor(restrict_xpaths =r'//ul[@class = "menu-nav"]/li/a[contains(concat(" ", normalize-space(@class), " "), " nav-link ") and not(contains(@class, "home"))]'), callback="parse_nav", follow=True),
+        ) 
     def parse_nav (self, response):
-        # article_rurls = response.xpath('//*[@id="content"]//h3[contains (@class, "box-title-text")]/a/@href').extract()
         category = response.url.split('/')[-1].split('.')[0]
-        # for article_rurl in article_rurls[:3]:
-        #     article_url = urllib.parse.urljoin (response.url, article_rurl)
-        #     yield response.follow (article_url, callback = self.parse_article, meta = {'category' : category})
         for link in LinkExtractor(restrict_xpaths =r'//*[@id="content"]//h3[contains (@class, "box-title-text")]/a').extract_links(response):
             yield response.follow (link, callback = self.parse_article,meta = {'category': category})
     
     def parse_article (self, response):
         article_item = ArticleItem()
         article_item['url'] = response.url
-        article_item['title'] = response.css ('h1.detail-title span::text').get()
+        article_item['title'] = response.xpath ('//*[@id="content"]//h1/span/text()').extract_first()
         category = response.meta['category']
         article_item['category'] = category
-        article_item['author_name'] = response.css('.detail-author .author-info-top a::text').get()
-        article_item['author_email'] = response.css ('.detail-author .author-info-top span.email::text').get()
-        article_item['publish_date'] = response.css(' div[data-role="publishdate"]::text').get()
+        article_item['author_name'] = response.xpath ('//*[@id="content"]//div[@class="author-info-top"]/a/text()').extract_first()
+        article_item['author_email'] = response.xpath ('//*[@id="content"]//div[@class="author-info-top"]//span[@class="email"]/text()').extract_first()
+        article_item['publish_date'] = response.xpath ('//*[@id="content"]//div[@class="detail-time"]/div[@data-role="publishdate"]/text()').extract_first()
         yield article_item 
